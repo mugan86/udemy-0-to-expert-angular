9dc4fb038caa550133e8f7bbb21df2999ce2ef59
Anartz Mugika <mugan86@gmail.com>
2018-05-30 20:03:53 +0200

    Section 4 Video 49 Finish

--- 2.- spa/src/app/app.routes.ts
+++ 2.- spa/src/app/app.routes.ts
@@ -1,9 +1,13 @@
 import { RouterModule,  Routes } from '@angular/router';
 import { ModuleWithProviders } from '@angular/core';
 import { HomeComponent } from './components/home/home.component';
+import { HeroesComponent } from './components/heroes/heroes.component';
+import { AboutComponent } from './components/about/about.component';
 
 const APP_ROUTES: Routes = [
     { path: 'home', component: HomeComponent},
+    { path: 'about', component: AboutComponent },
+    { path: 'heroes', component: HeroesComponent },
     { path: '**', pathMatch: 'full' , redirectTo: 'home' },
 ];
 
