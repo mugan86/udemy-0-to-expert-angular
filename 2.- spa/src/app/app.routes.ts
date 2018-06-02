0ea25dfd25124b7c8a015789b16a99c1c9e5d8ec
Anartz Mugika <mugan86@gmail.com>
2018-06-02 17:03:25 +0200

    Adapt to publish in shared hosting

--- 2.- spa/src/app/app.routes.ts
+++ 2.- spa/src/app/app.routes.ts
@@ -15,4 +15,4 @@ const APP_ROUTES: Routes = [
     { path: '**', pathMatch: 'full' , redirectTo: 'home' },
 ];
 
-export const APP_ROUTING: ModuleWithProviders = RouterModule.forRoot(APP_ROUTES);
+export const APP_ROUTING: ModuleWithProviders = RouterModule.forRoot(APP_ROUTES, { useHash: true});
