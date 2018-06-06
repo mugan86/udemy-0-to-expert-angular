b7858f9ca76bbe705c22189a49ff96a4622a7f93
Anartz Mugika <mugan86@gmail.com>
2018-06-06 20:02:05 +0200

    Section 8 Video 138. ngSwith to multiple decisions

--- 06_miscellaneousApp/src/app/app.module.ts
+++ 06_miscellaneousApp/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { NgStyleComponent } from './components/ng-style/ng-style.component';
 import { CssComponent } from './components/css/css.component';
 import { ClassesComponent } from './components/classes/classes.component';
 import { HighlightedDirective } from './directives/highlighted.directive';
+import { NgSwitchComponent } from './components/ng-switch/ng-switch.component';
 
 @NgModule({
   declarations: [
@@ -13,7 +14,8 @@ import { HighlightedDirective } from './directives/highlighted.directive';
     NgStyleComponent,
     CssComponent,
     ClassesComponent,
-    HighlightedDirective
+    HighlightedDirective,
+    NgSwitchComponent
   ],
   imports: [
     BrowserModule
---
+++ 06_miscellaneousApp/src/app/components/ng-switch/ng-switch.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ng-switch',
+  templateUrl: './ng-switch.component.html',
+  styles: []
+})
+export class NgSwitchComponent implements OnInit {
+
+  alert = 'info';
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
