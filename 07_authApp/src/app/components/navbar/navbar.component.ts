4fa1099a47c773a210d7be170da014101e0a2fab
Anartz Mugika <mugan86@gmail.com>
2018-06-08 20:02:14 +0200

    Section 9 Video 151. Add new functions, logout and manage authenticate with check in login/logout buttons

--- 07_authApp/src/app/components/navbar/navbar.component.ts
+++ 07_authApp/src/app/components/navbar/navbar.component.ts
@@ -8,13 +8,19 @@ import { AuthService } from '../../services/auth.service';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor( private auth: AuthService) { }
+  constructor( private auth: AuthService) {
+    auth.handleAuthentication();
+  }
 
   ngOnInit() {
   }
 
   login() {
     this.auth.login();
+  }
+
+  logout() {
+    this.auth.logout();
   }
 
 }
--- 07_authApp/src/app/services/auth.service.ts
+++ 07_authApp/src/app/services/auth.service.ts
@@ -25,4 +25,42 @@ export class AuthService {
     this.auth0.authorize();
   }
 
+  public handleAuthentication(): void {
+    this.auth0.parseHash((err, authResult) => {
+      if (authResult && authResult.accessToken && authResult.idToken) {
+        window.location.hash = '';
+        this.setSession(authResult);
+        this.router.navigate(['/home']);
+      } else if (err) {
+        this.router.navigate(['/home']);
+        console.log(err);
+      }
+    });
+  }
+
+  private setSession(authResult): void {
+    // Set the time that the Access Token will expire at
+    const expiresAt = JSON.stringify((authResult.expiresIn * 1000) + new Date().getTime());
+    localStorage.setItem('access_token', authResult.accessToken);
+    localStorage.setItem('id_token', authResult.idToken);
+    localStorage.setItem('expires_at', expiresAt);
+  }
+
+  public logout(): void {
+    // Remove tokens and expiry time from localStorage
+    localStorage.removeItem('access_token');
+    localStorage.removeItem('id_token');
+    localStorage.removeItem('expires_at');
+    // Go back to the home route
+    this.router.navigate(['/']);
+  }
+
+  public isAuthenticated(): boolean {
+    // Check whether the current time is past the
+    // Access Token's expiry time
+    const expiresAt = JSON.parse(localStorage.getItem('expires_at') || '{}');
+    return new Date().getTime() < expiresAt;
+  }
+
+
 }
