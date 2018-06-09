eefa18602c2a753de60e5d4b534b245e5ee951a2
Anartz Mugika <mugan86@gmail.com>
2018-06-09 09:48:40 +0200

    Section 9 Video 153. Take login profile data and navigate only to protect section if user session start

--- 07_authApp/src/app/components/protect/protect.component.ts
+++ 07_authApp/src/app/components/protect/protect.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from './../../services/auth.service';
 
 @Component({
   selector: 'app-protect',
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProtectComponent implements OnInit {
 
-  constructor() { }
+  constructor(private auth: AuthService) { }
 
   ngOnInit() {
+    if (this.auth.userProfile) {
+      this.profile = this.auth.userProfile;
+    } else {
+      this.auth.getProfile((err, profile) => {
+        this.profile = profile;
+        console.log(this.profile);
+      });
+    }
+    
   }
 
 }
--- 07_authApp/src/app/services/auth.service.ts
+++ 07_authApp/src/app/services/auth.service.ts
@@ -14,8 +14,10 @@ export class AuthService {
     responseType: 'token id_token',
     audience: 'https://mugan86.eu.auth0.com/userinfo',
     redirectUri: 'http://localhost:4200/callback',
-    scope: 'openid'
+    scope: 'openid profile'
   });
+
+  public userProfile: any;
 
   constructor(public router: Router) { }
 
@@ -59,6 +61,21 @@ export class AuthService {
     const expiresAt = JSON.parse(localStorage.getItem('expires_at') || '{}');
     return new Date().getTime() < expiresAt;
   }
+
+  public getProfile(cb): void {
+    const accessToken = localStorage.getItem('access_token');
+    if (!accessToken) {
+      throw new Error('Access Token must exist to fetch profile');
+    }
+
+    const self = this;
+    this.auth0.client.userInfo(accessToken, (err, profile) => {
+      if (profile) {
+        self.userProfile = profile;
+      }
+      cb(err, profile);
+    });
+}
 
 
 }
