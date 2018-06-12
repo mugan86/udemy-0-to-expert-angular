92164ecf066bdc9455879cdd6120182cac9427d8
Anartz Mugika <mugan86@gmail.com>
2018-06-12 20:09:22 +0200

    Section 5 Pipes - Video 74-78 Percent, currency, json, async and date pipe change to spanish

--- 03_pipes/src/app/app.component.ts
+++ 03_pipes/src/app/app.component.ts
@@ -9,4 +9,20 @@ export class AppComponent {
   name = 'Anartz';
   numbersList = Array.from(Array(29).keys()); // [0,...,28]
   PI = Math.PI;
+  a = 0.259;
+  b = 1.3495;
+  money = 123456.4;
+  hero = {
+    name: 'Logan',
+    age: 500,
+    key: 'Wolverine',
+    address: {
+      street: 'First',
+      number: '19'
+    }
+  };
+
+  promiseValue = new Promise( ( resolve, reject) => {
+    setTimeout( () => resolve('Take data!!'), 3500 );
+  });
 }
--- 03_pipes/src/app/app.module.ts
+++ 03_pipes/src/app/app.module.ts
@@ -2,6 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { registerLocaleData } from '@angular/common';
+import localeFr from '@angular/common/locales/fr';
+
+// the second parameter 'fr' is optional
+registerLocaleData(localeFr, 'fr');
+registerLocaleData(localeFr, 'es');
 
 @NgModule({
   declarations: [
