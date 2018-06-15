bd3a6a3b2e76561c7308644d8b45969b20d69b1b
Anartz Mugika <mugan86@gmail.com>
2018-06-15 18:18:21 +0200

    Section 11 Video 185 Finish

--- 09_heroesapp/src/app/components/heroes/heroe.component.ts
+++ 09_heroesapp/src/app/components/heroes/heroe.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { NgForm } from '@angular/forms';
 import { Heroe } from './../../interfaces/heroe.interface';
 
 @Component({
--- 09_heroesapp/src/app/components/heroes/heroes.component.ts
+++ 09_heroesapp/src/app/components/heroes/heroes.component.ts
@@ -16,5 +16,4 @@ export class HeroesComponent implements OnInit {
 
   ngOnInit() {
   }
-
 }
