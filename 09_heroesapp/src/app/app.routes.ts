9e1535895dcd1fbb968915fb66034a69b1d7082f
Anartz Mugika <mugan86@gmail.com>
2018-06-15 07:17:38 +0200

    Section 11 Video 185 Finish - Create main heroes list page and add hero form

--- 09_heroesapp/src/app/app.routes.ts
+++ 09_heroesapp/src/app/app.routes.ts
@@ -6,7 +6,7 @@ import { HeroeComponent } from './components/heroes/heroe.component';
 export const APP_ROUTES: Routes = [
     { path: 'heroes', component: HeroesComponent},
     { path: 'heroe/:id', component: HeroeComponent },
-    { path: '**', pathMatch: 'full' , redirectTo: 'home' },
+    { path: '**', pathMatch: 'full' , redirectTo: 'heroes' },
 ];
 
 export const APP_ROUTING: ModuleWithProviders = RouterModule.forRoot(APP_ROUTES, { useHash: true});
--- 09_heroesapp/src/app/components/heroes/heroes.component.ts
+++ 09_heroesapp/src/app/components/heroes/heroes.component.ts
@@ -3,7 +3,12 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
-  styles: []
+  styles: [
+    ` .table-responsive {
+        margin-top: 15px;
+      }
+    `
+  ]
 })
 export class HeroesComponent implements OnInit {
 
