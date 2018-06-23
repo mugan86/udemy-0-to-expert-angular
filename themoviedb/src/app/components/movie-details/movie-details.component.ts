6cc90e53d7d8910bd39a9a0510c035f841f8c926
Anartz Mugika <mugan86@gmail.com>
2018-06-23 19:05:49 +0200

    Manage Movie details to print in template

--- themoviedb/src/app/components/movie-details/movie-details.component.ts
+++ themoviedb/src/app/components/movie-details/movie-details.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Movie } from './../../interfaces/movie.interface';
+import { TheMovieDbApiService } from './../../providers/the-movie-db-api.service';
 
 @Component({
   selector: 'app-movie-details',
@@ -6,10 +9,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./movie-details.component.css']
 })
 export class MovieDetailsComponent implements OnInit {
-
-  constructor() { }
+  movie: Movie;
+  beforePage: string;
+  constructor(private router: ActivatedRoute,
+              public _mtvshows: TheMovieDbApiService,
+              private route: Router) { }
 
   ngOnInit() {
+    this.router.params.subscribe(params => {
+      console.log(params['id'], params['page']);
+      this.beforePage = params['page'];
+      this._mtvshows.getMovie(params['id']).subscribe((res: Movie) => {
+        this.movie = res;
+        console.log(this.movie);
+      })
+    });
+    
+  }
+
+  returnToBeforePage() {
+    console.log('return', this.beforePage);
+    this.route.navigate([`/${this.beforePage}`]);
   }
 
 }
