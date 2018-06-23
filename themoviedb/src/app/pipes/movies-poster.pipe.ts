e657c3d8e52f90cbc52cfaf3f7154367a97d1bcb
Anartz Mugika <mugan86@gmail.com>
2018-06-23 19:08:04 +0200

    Update pipe poster movie to use in movie details

--- themoviedb/src/app/pipes/movies-poster.pipe.ts
+++ themoviedb/src/app/pipes/movies-poster.pipe.ts
@@ -5,8 +5,12 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class MoviesPosterPipe implements PipeTransform {
 
-  transform(movie: any): string {
-    const movieDbImagePathUrl = 'https://image.tmdb.org/t/p/w300';
+  transform(movie: any, poster: boolean = false): string {
+    const movieDbImagePathUrl = 'https://image.tmdb.org/t/p/w500';
+
+    if ( poster ) {
+      return `${movieDbImagePathUrl}${movie.poster_path}`;
+    }
     if (movie.backdrop_path) {
       return `${movieDbImagePathUrl}${movie.backdrop_path}`;
     } else if (movie.poster_path) {
