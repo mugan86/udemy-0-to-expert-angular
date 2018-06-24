6ba4783e9173828ce93f65ab86427cb927926132
Anartz Mugika <mugan86@gmail.com>
2018-06-24 08:27:21 +0200

    Add to search trailer and pass search text in route

--- themoviedb/src/app/components/movie-details/movie-details.component.ts
+++ themoviedb/src/app/components/movie-details/movie-details.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Movie } from './../../interfaces/movie.interface';
 import { TheMovieDbApiService } from './../../providers/the-movie-db-api.service';
+import { YoutubeApiService } from '../../providers/youtube-api.service';
 
 @Component({
   selector: 'app-movie-details',
@@ -10,18 +11,31 @@ import { TheMovieDbApiService } from './../../providers/the-movie-db-api.service
 })
 export class MovieDetailsComponent implements OnInit {
   movie: Movie;
+  trailer: any;
   beforePage: string;
+  searchText = '';
   constructor(private router: ActivatedRoute,
               public _mtvshows: TheMovieDbApiService,
+              private youtubeApi: YoutubeApiService,
               private route: Router) { }
 
   ngOnInit() {
     this.router.params.subscribe(params => {
       console.log(params['id'], params['page']);
       this.beforePage = params['page'];
+      if (params['search']) {
+        this.searchText = params['search'];
+      }
       this._mtvshows.getMovie(params['id']).subscribe((res: Movie) => {
         this.movie = res;
         console.log(this.movie);
+        this.youtubeApi.getSearchVideos(`${this.movie.title} (${this.movie.release_date.substring(0,4)})`).subscribe((res: any) => {
+          if (res[0]) {
+            console.log(res[0]);
+            this.trailer = res[0];
+            console.log(res[0].id.videoId);
+          }
+        })
       })
     });
     
@@ -29,7 +43,12 @@ export class MovieDetailsComponent implements OnInit {
 
   returnToBeforePage() {
     console.log('return', this.beforePage);
-    this.route.navigate([`/${this.beforePage}`]);
+    if (this.searchText != '') {
+      this.route.navigate([`/${this.beforePage}`, this.searchText]);
+    } else {
+      this.route.navigate([`/${this.beforePage}`]);
+    }
+    
   }
 
 }
