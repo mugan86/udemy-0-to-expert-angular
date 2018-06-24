a81327bb8a0ec58fb3f0be570ec0715033018d76
Anartz Mugika <mugan86@gmail.com>
2018-06-24 08:20:37 +0200

    Update with new pipes, services and components

--- themoviedb/src/app/app.module.ts
+++ themoviedb/src/app/app.module.ts
@@ -3,20 +3,27 @@ import { NgModule } from '@angular/core';
 import { HttpClientJsonpModule, HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 
+// Routing
+import { APP_ROUTING } from './app.routes';
+
+// Pipes
+import { DomSecurePipe } from './pipes/dom-secure.pipe';
+import { YoutubePipe } from './pipes/youtube.pipe';
+import { MoviesPosterPipe } from './pipes/movies-poster.pipe';
+
+// Services
+import { TheMovieDbApiService } from './providers/the-movie-db-api.service';
+import { YoutubeApiService } from './providers/youtube-api.service';
+
+// Conponents
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { MovieDetailsComponent } from './components/movie-details/movie-details.component';
 import { SearchComponent } from './components/search/search.component';
 import { NavbarComponent } from './components/shared/navbar/navbar.component';
-
-import { APP_ROUTING } from './app.routes';
-
-
-// Services
-import { TheMovieDbApiService } from './providers/the-movie-db-api.service';
-import { MoviesPosterPipe } from './pipes/movies-poster.pipe';
 import { GalleryComponent } from './widgets/gallery/gallery.component';
 import { PaginationComponent } from './components/shared/pagination/pagination.component';
+import { YoutubeVideoComponent } from './components/youtube-video/youtube-video.component';
 
 @NgModule({
   declarations: [
@@ -27,7 +34,10 @@ import { PaginationComponent } from './components/shared/pagination/pagination.c
     NavbarComponent,
     MoviesPosterPipe,
     GalleryComponent,
-    PaginationComponent
+    PaginationComponent,
+    DomSecurePipe,
+    YoutubePipe,
+    YoutubeVideoComponent
   ],
   imports: [
     APP_ROUTING,
@@ -36,7 +46,7 @@ import { PaginationComponent } from './components/shared/pagination/pagination.c
     HttpClientModule,
     HttpClientJsonpModule
   ],
-  providers: [ TheMovieDbApiService ],
+  providers: [ TheMovieDbApiService, YoutubeApiService ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
