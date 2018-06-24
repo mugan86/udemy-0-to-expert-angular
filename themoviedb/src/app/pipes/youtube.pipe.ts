227d5ac39d0829887015eeb7e52c7b955b785dfe
Anartz Mugika <mugan86@gmail.com>
2018-06-24 17:32:46 +0200

    Add documentation in pipe

--- themoviedb/src/app/pipes/youtube.pipe.ts
+++ themoviedb/src/app/pipes/youtube.pipe.ts
@@ -7,6 +7,11 @@ import { DomSanitizer } from '@angular/platform-browser';
 })
 export class YoutubePipe implements PipeTransform {
   constructor(private domSanitizer: DomSanitizer) {}
+  /**
+   * Return correctly youtube video player embed to ready to show in template
+   * @param value Youtube Video ID value
+   * @example If take https://www.youtube.com/watch?v=UZFeOJHGU_Y video, the pass value is 'UZFeOJHGU_Y'
+   */
   transform(value: any): any {
     return new DomSecurePipe(this.domSanitizer).transform(value, 'https://www.youtube.com/embed/') ;
   }
