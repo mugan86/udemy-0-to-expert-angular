d786de3189b4282452e219a45385f3cee10bf395
Anartz Mugika <mugan86@gmail.com>
2018-06-30 19:59:34 +0200

    Add new modules, componentes and services

--- 16_ng-charts-app/src/app/app.module.ts
+++ 16_ng-charts-app/src/app/app.module.ts
@@ -1,20 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { ChartsModule } from 'ng2-charts';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { GraphicLinesComponent } from './components/graphic-lines/graphic-lines.component';
+import { BarChartsComponent } from './components/bar-charts/bar-charts.component';
+import { DonaChartsComponent } from './components/dona-charts/dona-charts.component';
+import { RadarChartsComponent } from './components/radar-charts/radar-charts.component';
+
+// Services
+import { FilesLoadService } from './services/files-load.service';
 
 @NgModule({
   declarations: [
     AppComponent,
-    GraphicLinesComponent
+    GraphicLinesComponent,
+    BarChartsComponent,
+    DonaChartsComponent,
+    RadarChartsComponent
   ],
   imports: [
     BrowserModule,
-    ChartsModule
+    ChartsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [ FilesLoadService ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
