f0acf112e22ce180cf29ca85c5539e39dfa55ab1
Anartz Mugika <mugan86@gmail.com>
2018-07-01 17:49:02 +0200

    Add dinamically load file

--- 16_ng-charts-app/src/app/services/files-load.service.ts
+++ 16_ng-charts-app/src/app/services/files-load.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { map } from 'rxjs/internal/operators/map';
 import { Observable } from 'rxjs/internal/Observable';
 
 
@@ -9,7 +8,7 @@ export class FilesLoadService {
 
   constructor(private http: HttpClient) { }
 
-  public getJSON(): Observable<any> {
-    return this.http.get('./assets/activities/11_08_2017_malgrat.json');
+  public getJSON(activityName: string): Observable<any> {
+    return this.http.get(`./assets/activities/${activityName}.json`);
   }
 }
