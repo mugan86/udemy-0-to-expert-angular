f91aeb8ed00ccead81665234b3310748043b944c
Anartz Mugika <mugan86@gmail.com>
2018-09-03 18:55:13 +0200

    Validations in dropngzone directive with files

--- 15_fileuploadFirebase/src/app/directives/ng-drop-zones.directive.ts
+++ 15_fileuploadFirebase/src/app/directives/ng-drop-zones.directive.ts
@@ -1,4 +1,6 @@
 import { Directive , EventEmitter, ElementRef, HostListener, Input, Output} from '@angular/core';
+import { FileItem } from '../models/file-item';
+import { ngDevModeResetPerfCounters } from '@angular/core/src/render3/ng_dev_mode';
 
 // EventEmitter, para notificar sobre sucesos.
 // HostListener, para escuchar eventos del ratón
@@ -6,6 +8,8 @@ import { Directive , EventEmitter, ElementRef, HostListener, Input, Output} from
   selector: '[appNgDropZones]'
 })
 export class NgDropZonesDirective {
+
+  @Input() files: FileItem[];
   @Output()
   mouseOver: EventEmitter<boolean> = new EventEmitter();
   constructor() {}
@@ -24,5 +28,36 @@ export class NgDropZonesDirective {
   @HostListener('dragleave', ['$event'])
   public onDragLeave(event: any) {
     this.mouseOver.emit(false);
+  }
+
+  // Validations
+
+  private _preventAndPause ( event ) {
+    event.preventDefault();
+    event.stopPropagation();
+  }
+
+  private _fileCanLoad( file: File ): boolean {
+    if ( !this._fileAlreadyAdd (file.name) &&
+          this._isImage ( file.type )) {
+            return true;
+    }
+    return false;
+  }
+
+  private _fileAlreadyAdd( fileName: string ): boolean {
+    for (const file of this.files ) {
+      if (file.archiveName === fileName) {
+        console.log(`The file ${fileName} already add`);
+        return true;
+      }
+    }
+    return false;
+  }
+
+  // Check if drop file is image
+
+  private _isImage( fileType: string ): boolean {
+    return ( fileType === '' || fileType === undefined ) ? false : fileType.startsWith('image');
   }
 }
