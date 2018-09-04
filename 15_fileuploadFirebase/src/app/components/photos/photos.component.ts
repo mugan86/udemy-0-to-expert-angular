2139c820647892e912e9966aa4c4d48f6d3371cc
Anartz Mugika <mugan86@gmail.com>
2018-09-04 13:23:05 +0200

    FInish course

--- 15_fileuploadFirebase/src/app/components/photos/photos.component.ts
+++ 15_fileuploadFirebase/src/app/components/photos/photos.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/internal/Observable';
+import { AngularFirestoreCollection, AngularFirestore } from 'angularfire2/firestore';
+
+export interface Item {
+  nombre: string;
+  url: string;
+}
 
 @Component({
   selector: 'app-photos',
@@ -6,10 +13,13 @@ import { Component, OnInit } from '@angular/core';
   styles: []
 })
 export class PhotosComponent implements OnInit {
+  private itemsCollection: AngularFirestoreCollection<Item>;
+  items: Observable<Item[]>;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private afs: AngularFirestore) {
+    this.itemsCollection = afs.collection<Item>('img');
+    this.items = this.itemsCollection.valueChanges();
   }
 
+  ngOnInit() {}
 }
